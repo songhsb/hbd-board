3b402519ab317d453e47dbb87186ee4a7cf3f01b
songhsb <hssong404@gmail.com>
2023-08-03 19:32:51 +0900

    Feat: AuthLayout 완료

--- src/shared/AuthLayout.tsx
+++ src/shared/AuthLayout.tsx
@@ -8,13 +8,21 @@ const AuthLayout = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    alert("TODO 요구사항에 맞추어 기능을 완성해주세요.");
+    // alert("TODO 요구사항에 맞추어 기능을 완성해주세요.");
 
     // TODO: localStorage의 토큰 검색
     // TODO: localStorage의 이메일 검색
+    const token = localStorage.getItem("token");
+    const email = localStorage.getItem("email");
     // TODO: 토큰 또는 이메일 중 하나라도 없을 경우 "토큰 또는 이메일이 없습니다. 로그인해주세요." alert
     // TODO: localStorage에 있는 token, email을 제거
     // TODO: "/auth"로 이동
+    if (!email || !token) {
+      alert("토큰 또는 이메일이 없습니다. 로그인해주세요.");
+      localStorage.removeItem("token");
+      localStorage.removeItem("email");
+      navigate("/auth");
+    }
   }, [navigate]);
 
   const handleLogoutButtonClick = () => {
